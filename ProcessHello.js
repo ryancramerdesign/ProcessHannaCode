d0e8275892be0b90494b51444230518e8214ecd3
Ryan Cramer <ryan@rc-d.net>
2013-06-02 09:22:37 -0400

    first commit

---
+++ ProcessHello.js
@@ -0,0 +1,10 @@
+/**
+ * This JS file is only loaded when the ProcessHello module is run
+ *
+ * You should delete it if you have no javascript to add.
+ *
+ */
+
+$(document).ready(function() {
+	// do something
+}); 
