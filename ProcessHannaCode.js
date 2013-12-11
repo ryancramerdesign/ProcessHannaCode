0c654062b1dbfee344e700157c1b2f04f4182450
Ryan Cramer <ryan@rc-d.net>
2013-12-11 05:45:01 -0500

    Updates to correct extra whitespace appearing in Ace code, plus fix the issue that caused the Ace editor to not appear after running a "save and test".

--- ProcessHannaCode.js
+++ ProcessHannaCode.js
@@ -1,54 +1,56 @@
-$(document).ready(function() {
-	$("#hc_export").click(function() { $(this).select(); });
+function initHannaCodeAce() {
 
 	var hcCode = $("#hc_code"); 
-	if(hcCode.size() > 0) {
+	if(hcCode.size() < 1) return;
 
-		$('#HannaCodeEdit').WireTabs({
-			items: $(".WireTab"),
-			skipRememberTabIDs: ['HannaCodeDelete']
-			});
+	var hcCodeDiv = $('<div id="hc_code_div"></div>')
+		.css('height', hcCode.attr('data-height') + 'px'); 
 
+	hcCode.after(hcCodeDiv); 
+	var editor=ace.edit('hc_code_div');
 
-		var hcCodeDiv = $('<div id="hc_code_div"></div>')
-			.css('height', hcCode.attr('data-height') + 'px'); 
+	hcCode.hide();
 
-		hcCode.after(hcCodeDiv); 
-		var editor=ace.edit('hc_code_div');
+	editor.getSession().setValue(hcCode.val()); 	
+	editor.getSession().on('change', function() { 
+		hcCode.val(editor.getSession().getValue())
+	}); 
 
-		hcCode.hide();
+	editor.setTheme('ace/theme/' + hcCode.attr('data-theme')); 
 
-		editor.getSession().setValue(hcCode.val()); 	
-		editor.getSession().on('change', function() { 
-			hcCode.val(editor.getSession().getValue())
-		}); 
+	var hcType = $("input[name=hc_type]"); 
 
-		editor.setTheme('ace/theme/' + hcCode.attr('data-theme')); 
+	hcType.change(function() {
 
-		var hcType = $("input[name=hc_type]"); 
+		if(!$(this).is(":checked")) return;
 
-		hcType.change(function() {
+		var val = $(this).val();
+		var editorMode = 'html';
+		var editorValue = editor.getSession().getValue();
+		var phpBlankValue = "<?php\n\n";
 
-			if(!$(this).is(":checked")) return;
+		if(val == 1) {
+			editorMode = 'javascript';
+			if(editorValue == phpBlankValue) editor.getSession().setValue('');
+		} else if(val == 2) {
+			editorMode = 'php';
+			if(editorValue.length < 1) editor.getSession().setValue(phpBlankValue);
+		} else {
+			editorMode = 'plain_text';
+			if(editorValue == phpBlankValue) editor.getSession().setValue('');
+		}
 
-			var val = $(this).val();
-			var editorMode = 'html';
-			var editorValue = editor.getSession().getValue();
-			var phpBlankValue = "<?php\n\n";
+		editor.getSession().setMode("ace/mode/" + editorMode); 
 
-			if(val == 1) {
-				editorMode = 'javascript';
-				if(editorValue == phpBlankValue) editor.getSession().setValue('');
-			} else if(val == 2) {
-				editorMode = 'php';
-				if(editorValue.length < 1) editor.getSession().setValue(phpBlankValue);
-			} else {
-				editorMode = 'plain_text';
-				if(editorValue == phpBlankValue) editor.getSession().setValue('');
-			}
+	}).change();
+}
 
-			editor.getSession().setMode("ace/mode/" + editorMode); 
+$(document).ready(function() {
+	$("#hc_export").click(function() { $(this).select(); });
+	$('#HannaCodeEdit').WireTabs({
+		items: $(".WireTab"),
+		skipRememberTabIDs: ['HannaCodeDelete']
+		});
 
-		}).change();
-	}
+	setTimeout('initHannaCodeAce()', 250);
 }); 
