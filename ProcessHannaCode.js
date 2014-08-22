5f19ad2752a5e8c39f3b79462d987bea79a4aef0
Ryan Cramer <ryan@rc-d.net>
2014-08-22 11:12:28 -0400

    Various updates to ProcessHannaCode including switch to PDO and major upgrades to the Ace feature set, including live changes of theme, keybindings, and pairing/wrapping behaviors. Also added support for new permissions so that one can view Hanna Codes without being able to edit them, as well as a permission for editing PHP Hanna codes. For existing installations, create these permissions: hanna-code-edit, hanna-code-php. Once those permissions are present, the existing "hanna-code" permission is considered a view-only permission. If the new permissions aren't present, then the existing "hanna-code" permission does the same thing as before (full edit permission).

--- ProcessHannaCode.js
+++ ProcessHannaCode.js
@@ -16,7 +16,17 @@ function initHannaCodeAce() {
 		hcCode.val(editor.getSession().getValue())
 	}); 
 
-	editor.setTheme('ace/theme/' + hcCode.attr('data-theme')); 
+	var hcTheme = hcCode.attr('data-theme');
+	var _hcTheme = editor.getTheme();
+	if(hcTheme) editor.setTheme('ace/theme/' + hcTheme); 
+
+	var hcKeybinding = hcCode.attr('data-keybinding'); 
+	var _hcKeybinding = editor.getKeyboardHandler(); // original
+	if(hcKeybinding && hcKeybinding != 'none') editor.setKeyboardHandler('ace/keyboard/' + hcKeybinding);
+
+	var hcBehaviors = parseInt(hcCode.attr('data-behaviors')); 
+	editor.setBehavioursEnabled(hcBehaviors & 2); 
+	editor.setWrapBehavioursEnabled(hcBehaviors & 4); 
 
 	var hcType = $("input[name=hc_type]"); 
 
@@ -43,6 +53,38 @@ function initHannaCodeAce() {
 		editor.getSession().setMode("ace/mode/" + editorMode); 
 
 	}).change();
+
+	var setAceBehaviors = function() {
+		var behaviors = 0; 
+		$("#wrap_aceBehaviors input").each(function() {
+			var val = parseInt($(this).val());
+			var enabled = $(this).is(":checked");
+			if(val == 2) editor.setBehavioursEnabled(enabled); 
+			if(val == 4) editor.setWrapBehavioursEnabled(enabled); 
+			
+		}); 
+	};
+
+	$("#aceTheme").change(function() {
+		var val = $(this).val();
+		if(val.length == 0) editor.setTheme(_hcTheme); 
+			else editor.setTheme('ace/theme/' + $(this).val()); 
+	});
+	$("#aceHeight").change(function() {
+		var height = $(this).val();
+		if(height < 100) height = 100;
+		if(height > 2000) height = 2000; 
+		$("#hc_code_div").css('height', height + 'px'); 
+		editor.resize();
+	});
+	$("#aceKeybinding").change(function() {
+		var val = $(this).val();
+		if(val == 'none' || val.length == 0) editor.setKeyboardHandler(_hcKeybinding); 
+			else editor.setKeyboardHandler('ace/keyboard/' + $(this).val()); 
+	});
+	$("#wrap_aceBehaviors input").change(function() {
+		setTimeout(setAceBehaviors, 250); 
+	});
 }
 
 $(document).ready(function() {
--- README.md
+++ README.md
@@ -212,7 +212,7 @@ cIjtcclxufSBlbHNlIHtcclxuICBlY2hvICcnO1xyXG59In0=/!HannaCode
 
 
 ------
-Copyright 2013 by Ryan Cramer
+Copyright 2014 by Ryan Cramer
 
 
 
